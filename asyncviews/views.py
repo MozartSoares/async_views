458c63e628b734ead216cb9bac5a782ef01d7e0c
Mozart Soares <mozartarmstrong@gmail.com>
2024-04-19 21:47:18 -0300

    -adicionado resolução do exercicio

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -1,33 +1,29 @@
 import asyncio
 import httpx
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 async def http_call_async():
-  
-  #imprimindo números de 1 a 5 um por segundo para simular async
-  for num in range(1,6):
-    await asyncio.sleep(1)
-    print(num)
   async with httpx.AsyncClient() as client:
-    r = await client.get("https://httpbin.org")
-    print(r)
+    response = await client.get(url='https://fake-api-tau.vercel.app/api/efood/restaurantes')
+    restaurant_list = [restaurant for restaurant in response.json()]
+
+    for restaurant in restaurant_list:
+      await asyncio.sleep(1)
+      print(restaurant['titulo'])
+
+    
+      r = await client.get("https://httpbin.org")
+      print(r)
+
+  return restaurant_list
 
 async def async_view(request):
   
   #chama o event loop
   loop = asyncio.get_event_loop()
   
+  restaurant_list = await loop.create_task(http_call_async())
+  
   #cria a task 
   loop.create_task(http_call_async())
-  return HttpResponse("Non blocking http request")
-
-def http_call_sync():
-  for num in range(1,6):
-    asyncio.sleep(1)
-    print(num)
-  r = httpx.get("https://httpbin.org")
-  print(r)
-
-def sync_view(request):
-  http_call_sync()
-  return HttpResponse("Blocking http request")
+  return JsonResponse(restaurant_list, safe=False)
